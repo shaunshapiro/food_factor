b64d6cf0d75bf732b07786d68658e449d9eb7988
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-09-22 03:10:01 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodFactor::Application.config.secret_key_base = '95f1049bba6c80406bdfd6e4305dee5999554be45eabce18d8bd92e6c995f5875be22e92e09d2649b5fb1c96e49149a0439928827d1648fc9aa7de51a54a4dbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodFactor::Application.config.session_store :cookie_store, key: '_food_factor_session'
